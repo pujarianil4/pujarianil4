31dfe3cc040beaad651f03a31609cb5c1d7b8573
Anil Pujari <pujarianil4@gmail.com>
2021-07-12 10:50:04 +0530

    background imgae

--- my-portfolio/src/Pages/Home/Home.jsx
+++ my-portfolio/src/Pages/Home/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import styles from "./Home.module.css"
 const Home = () => {
+const [theme,setTheme]=useState("dark")
+    const dark={
+        color:"white"
+    }
+const light={
+    color:"black"
+}
+    const bg=theme==="dark"?"dark.png":"white.png"
     return (
-        <div>
-            <h1>Hi Dashboard</h1>
+        <div style={theme==="dark"?dark:light} className={styles.home}>
+             <img src={bg} alt="" />
+           <div className={styles.title} >
+               <h1 onClick={()=>setTheme(theme==="dark"?"light":"dark") }>Hi </h1>
+           </div>
+           
         </div>
     );
 };
