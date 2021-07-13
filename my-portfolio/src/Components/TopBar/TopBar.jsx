2877bc95bc53abde2746ab137d81155032c1c37c
Anil Pujari <pujarianil4@gmail.com>
2021-07-13 14:11:00 +0530

    themehandler in topbar

--- my-portfolio/src/Components/TopBar/TopBar.jsx
+++ my-portfolio/src/Components/TopBar/TopBar.jsx
@@ -2,19 +2,28 @@ import React from 'react';
 import styles from "./TopBar.module.css"
 import CloseIcon from '@material-ui/icons/Close';
 import FormatColorTextIcon from "@material-ui/icons/FormatColorText";
+import FlareIcon from '@material-ui/icons/Flare';
+import NightsStayIcon from '@material-ui/icons/NightsStay';
 const areEqual=(prev,next)=>{
-    if(prev.topbar===next.topbar){
+
+    if(prev.topbar===next.topbar ){
         return true
     } else{
         return false
     }
+
+   
 }
 
 const TopBar = React.memo(({theme,setTheme,handleTopBar,topbar}) => {
-    console.log(topbar);
+
+  const handletheme=(tm)=>{
+    handleTopBar()
+    setTheme(tm)
+  }
 
     return (
-        <div style={topbar?{top:"0px"}:{}} className={styles.topbar}>
+        <div style={topbar?{top:"0px"}:{}} className={theme==="dark"?styles.dark_topbar:styles.light_topbar}>
                 {theme === "dark" ? (
           <FormatColorTextIcon
             style={{ fontSize: 50 }}
@@ -26,7 +35,25 @@ const TopBar = React.memo(({theme,setTheme,handleTopBar,topbar}) => {
             className={styles.dark_logo}
           />
         )}
-           <CloseIcon onClick={handleTopBar}    style={{ fontSize: 50 }} />
+        {theme === "dark" ? (
+          <CloseIcon
+            style={{ fontSize: 50 }}
+            className={styles.white_Close_logo}
+            onClick={handleTopBar}
+          />
+        ) : (
+          <CloseIcon
+            style={{ fontSize: 50 }}
+            className={styles.dark_Close_logo}
+            onClick={handleTopBar}
+          />
+        )}
+       
+           {theme==="light"? <FlareIcon fontSize="large" className={styles.btn} onClick={()=>  handletheme("dark")}/>
+       : <NightsStayIcon fontSize="large" className={styles.btn} onClick={()=> handletheme("light")} />
+            } 
+          
+           
         </div>
     );
 },areEqual)
