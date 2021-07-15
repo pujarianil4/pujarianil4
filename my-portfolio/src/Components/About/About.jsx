19892fc4fe3649e18f4757e3503595afc5bb224c
Anil Pujari <pujarianil4@gmail.com>
2021-07-15 13:25:51 +0530

    skills &  tools section

--- my-portfolio/src/Components/About/About.jsx
+++ my-portfolio/src/Components/About/About.jsx
@@ -1,30 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./About.module.css";
 import SendIcon from '@material-ui/icons/Send';
 import { Button }from "@material-ui/core"
-
+import { useDencrypt } from "use-dencrypt-effect";
 
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   send_dark: {
     fontWeight:600,
-    width: "100%"
+    backgroundColor:"#F2F2F2"
   },
   send_light:{
-      color: "white"
+    fontWeight:600,
+      color: "white",
+      backgroundColor:"#111111"
   }
 });
 const About = ({ theme }) => {
+  const { result, dencrypt } = useDencrypt();
     const classes=useStyles()
   const style =
     theme === "dark"
       ? { backgroundColor: "#111111",color:"white" }
       : { backgroundColor: "#F2F2F2",color:"#616161" };
+
+      useEffect(()=>{
+        dencrypt("Hi there");
+      },[])
   return (
     <div style={style} className={styles.container}>
       <div className={styles.about}>
-      <h1>Hi there</h1>
+      <h1 style={theme==="dark"?{}:{color:"black"}}>{result}</h1>
         <div className={styles.info}>
           <div>
           
@@ -35,7 +42,7 @@ const About = ({ theme }) => {
             interfaces. I take great care in the experience, architecture, and
             code quality of the things I build.
           </p>
-            <Button className={theme==="dark"?classes.send_light:classes.send_dark} startIcon={<SendIcon/>}>Send me a message </Button>
+            <Button variant="contained" className={theme==="dark"?classes.send_light:classes.send_dark} startIcon={<SendIcon/>}>Send me a message </Button>
           </div>
           <div>
               <img src="profile.jpg" alt="profile" />
---
+++ my-portfolio/src/Components/Skills/Skills.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect } from "react";
+import styles from "./Skills.module.css";
+import { useDencrypt } from "use-dencrypt-effect";
+const Skills = ({ theme }) => {
+  const { result, dencrypt } = useDencrypt();
+  const style =
+    theme === "dark"
+      ? { backgroundColor: "#111111", color: "white" }
+      : { backgroundColor: "#F2F2F2", color: "#616161" };
+
+  useEffect(() => {
+    dencrypt("Skills & Tools");
+  }, []);
+  return (
+    <div style={style} className={styles.container}>
+      <div className={styles.skills}>
+        <h1 style={theme === "dark" ? {} : { color: "black" }}>{result}</h1>
+
+        <div
+          className={theme === "dark" ? styles.light_cards : styles.dark_cards}
+        >
+          <div className={styles.grid_item}>
+            <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png" />
+            <div className={theme==="dark"?styles.dark_rib:styles.light_rib}>
+              <h2>GIT</h2>
+            </div>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png" />
+            <div className={theme==="dark"?styles.dark_rib:styles.light_rib}>
+              <h2>HTML</h2>
+            </div>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png" />
+            <div className={theme==="dark"?styles.dark_rib:styles.light_rib}>
+              <h2>CSS</h2>
+            </div>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png" />
+            <div className={theme==="dark"?styles.dark_rib:styles.light_rib}>
+              <h2>JAVASCRIPT</h2>
+            </div>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png" />
+            <div className={theme==="dark"?styles.dark_rib:styles.light_rib}>
+              <h2>REACTJS</h2>
+            </div>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nodejs/nodejs.png" />
+            <div className={theme==="dark"?styles.dark_rib:styles.light_rib}>
+              <h2>NODEJS</h2>
+            </div>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/json/json.png" />
+            <div className={theme==="dark"?styles.dark_rib:styles.light_rib}>
+              <h2>JSON</h2>
+            </div>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/express/express.png" />
+            <div className={theme==="dark"?styles.dark_rib:styles.light_rib}>
+              <h2>EXPRESS</h2>
+            </div>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png" />
+            <div className={theme==="dark"?styles.dark_rib:styles.light_rib}>
+              <h2>MONGODB</h2>
+            </div>
+          </div>
+          <div className={styles.grid_item}>
+            <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/firebase/firebase.png" />
+            <div className={theme==="dark"?styles.dark_rib:styles.light_rib}>
+              <h2>FIREBASE</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
