9877e22c70c4ffa43ac53a60cc07d46014b06741
Anil Pujari <pujarianil4@gmail.com>
2021-09-20 12:45:36 +0530

    test

--- my-portfolio/src/Redux/Action.js
+++
@@ -1,9 +0,0 @@
-import { THEME_HANDLER } from "./ActionTypes"
-
-
-export const themeHandler=()=>{
-    console.log("action");
-    return{
-        type:THEME_HANDLER
-    }
-}
--- my-portfolio/src/Redux/ActionTypes.js
+++
@@ -1,1 +0,0 @@
-export const THEME_HANDLER="THEME_HANDLER"
